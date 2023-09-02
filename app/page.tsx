c936f758b791250a7cda1c0ab8c96c5d5a644488
mohamadnaufalazim.mf <mohamadnaufalazim.mf@petronas.com>
2023-09-02 17:13:20 +0800

    Install react-icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Navbar from './components/Navbar'
+import LeftSide from './components/LeftSide'
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
       <Navbar />
 
       <div>
-        LeftSide
+        <LeftSide />
       </div>
 
       <div>
