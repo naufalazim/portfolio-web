269f13e018e1762ce16c12dde7fd8802fb7f2dfd
mohamadnaufalazim.mf <mohamadnaufalazim.mf@petronas.com>
2023-12-05 20:34:37 +0800

    fix about part

--- app/components/Achieve.tsx
+++ app/components/Achieve.tsx
@@ -17,7 +17,7 @@ const Achieve = () => {
             </h2>
 
             <p className='text-sm font-titleFont text-textGreen '>
-                My Project I Worked On
+                Project I Worked/Involved
             </p>
         </div>
         <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-10 lgl:px-10'>
