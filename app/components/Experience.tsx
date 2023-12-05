316292fad9c453bc3b223aff801275ac29460109
mohamadnaufalazim.mf <mohamadnaufalazim.mf@petronas.com>
2023-12-05 21:15:56 +0800

    update exp page 1.1v

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -3,22 +3,23 @@ import React, { useState } from 'react'
 import SectionTitle from './SectionTitle'
 import Petronas from './works/Petronas'
 import DSS from './works/DSS';
+import Silverlake from './works/Silverlake';
 
 const Experience = () => {
 
     // Add new state here: 
     const [workPetronas, setworkPetronas] = useState(true)
-    const [workDSS, setworkDSS] = useState(false)
+    const [workSilverlake, setworkSilverlake] = useState(false)
 
 
     const handlePet = () => {
+        setworkSilverlake(false);
         setworkPetronas(true);
-        setworkDSS(false);
     }
 
-    const handleDSS = () => {
+    const handleSilverlake = () => {
+        setworkSilverlake(true);
         setworkPetronas(false);
-        setworkDSS(true);
     }
 
   return <section 
@@ -33,6 +34,18 @@ const Experience = () => {
             {/* Add Company Title Here:  */}
 
             <li 
+            onClick={handleSilverlake} 
+            className={` 
+                ${workPetronas ? 
+                    " border-l-textGreen text-textGreen ": 
+                    " border-l-textDark text-textDark "
+                }  
+            border-l-2 border-l-textGreen text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm 
+            cursor-pointer duration-300 px-8 font-medium`}>
+                SILVERLAKE AXIS
+            </li>
+
+            <li 
             onClick={handlePet} 
             className={` 
                 ${workPetronas ? 
@@ -41,25 +54,14 @@ const Experience = () => {
                 }  
             border-l-2 border-l-textGreen text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm 
             cursor-pointer duration-300 px-8 font-medium`}>
-                Petronas
-            </li>
-
-            <li 
-            onClick={handleDSS} 
-            className={` ${workDSS ? 
-                " border-l-textGreen text-textGreen ": 
-                " border-l-textDark text-textDark "
-                }  
-             border-l-2 border-l-textGreen text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm 
-            cursor-pointer duration-300 px-8 font-medium`}>
-                DSS
+                PETRONAS
             </li>
 
         </ul>
 
         {/* When User Click It Shows The Content Below: */}
         {workPetronas && <Petronas />}
-        {workDSS && <DSS />}
+        {workSilverlake && <Silverlake />}
         
     </div>
   </section>
---
+++ app/components/works/Silverlake.tsx
@@ -0,0 +1,52 @@
+import {BiSolidRightArrow } from 'react-icons/bi'
+
+
+const Silverlake = () => {
+  return (
+    <div>
+        <h3 className="flex gap-1 font-medium text-xl font-titleFont">Junior Software Engineer @<span className='text-textGreen'>Silverlake Axis</span></h3>
+        <p className="text-sm mt-1 font-medium text-textDark">Sept 2023 - Present</p>
+
+        <ul className='mt-6 flex flex-col gap-3'>
+            <li className='text-base flex gap-2 text-textDark'>    
+                <span>
+                    <BiSolidRightArrow className=" text-textGreen mt-1" />
+                </span>
+                Project: Digital Core Möbius
+            </li>
+
+            <li className='text-base flex gap-2 text-textDark'>    
+                <span>
+                    <BiSolidRightArrow className=" text-textGreen mt-1" />
+                </span>
+                Spearheaded the development of Mobius Core Banking, a groundbreaking project that revolutionized banking processes.
+            </li>
+
+            <li className='text-base flex gap-2 text-textDark'>    
+                <span>
+                    <BiSolidRightArrow className=" text-textGreen mt-1" />
+                </span>
+                Engineered seamless API integration with SOAP API using cutting-edge technologies, with a primary focus on Spring Boot.
+            </li>
+
+            <li className='text-base flex gap-2 text-textDark'>    
+                <span>
+                    <BiSolidRightArrow className=" text-textGreen mt-1" />
+                </span>
+                Demonstrated expertise in handling and processing large volumes of data by implementing robust solutions with Spring Batch, ensuring optimal performance and efficiency.
+            </li>
+
+            <li className='text-base flex gap-2 text-textDark'>    
+                <span>
+                    <BiSolidRightArrow className=" text-textGreen mt-1" />
+                </span>
+                Played a pivotal role in designing and implementing highly efficient database schemas, significantly enhancing data storage and retrieval processes.
+            </li>
+
+         
+        </ul>
+    </div>
+  )
+}
+
+export default Silverlake
